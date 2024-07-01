56e3dca48ddcf4817e9693adb7141c6ab75414ee
manuelluevano <manuelluevano@hotmail.com>
2024-07-01 10:05:25 -0600

    Api CRUD accesorio - Agregamos Login y Auth 1.1

--- routes/accesorios.js
+++ routes/accesorios.js
@@ -9,7 +9,6 @@ router.post("/accesorio",check.auth, accesorioController.addAccesorio);
 router.get("/accesorios", check.auth, accesorioController.listAccesorio);
 router.get("/accesorio/:id", accesorioController.detail);
 
-router.post("/refaccion/:id",check.auth, refaccionController.addRefaccion);
 router.get("/accesorios/:busqueda",check.auth, accesorioController.buscador);
 router.patch("/accesorio/:id",check.auth,accesorioController.editar);
 
