8345756de5bd25496839a032004fdc05cf556f23
manuelluevano <manuelluevano@hotmail.com>
2024-06-30 23:46:53 -0600

    Api CRUD accesorio - Eliminamos Auth para test

--- routes/accesorios.js
+++ routes/accesorios.js
@@ -6,15 +6,15 @@ const accesorioController = require("../controllers/accesorios");
 const check = require("../middlewares/auth");
 
 router.post("/accesorio", accesorioController.addAccesorio);
-router.get("/accesorios", check.auth, accesorioController.listAccesorio);
+router.get("/accesorios",  accesorioController.listAccesorio);
 router.get("/accesorio/:id", accesorioController.detail);
 
 // router.post("/refaccion/:id", refaccionController.addRefaccion);
 router.get("/accesorios/:busqueda", accesorioController.buscador);
-router.patch("/accesorio/:id",check.auth,accesorioController.editar);
+// router.patch("/accesorio/:id",check.auth,accesorioController.editar);
 
 //VENTA DE ACCEOSIO
-router.post("/accesorio/:id",check.auth,accesorioController.ventaAccesorio);
+// router.post("/accesorio/:id",check.auth,accesorioController.ventaAccesorio);
 
 
 //Exportar router
