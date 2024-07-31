a9821c99a44dc718a5dd59af76ca9c8e70f64f5b
Manuel Luevano <manuelluevano@hotmail.com>
2024-07-30 23:25:18 -0600

    Prueba 5 'Error crash al crear accesorio por Cloudinary'

--- services/uploadImage.js
+++ services/uploadImage.js
@@ -1,15 +1,45 @@
-// var cloudinary = require("cloudinary").v2;
+var cloudinary = require("cloudinary").v2;
 
-// // const cloud_name = process.env.CLOUD_NAME;
-// // const api_key = process.env.API_KEY;
-// // const api_secret = process.env.API_SECRET;
+(async function() {
 
-// cloudinary.config({
-//   cloud_name: "dba0qjfeo",
-//   api_key: "483868386424955",
-//   api_secret: "Yi6UwgxL213LtudtiwIMZjgWQe8",
-// });
-
+    // Configuration
+    cloudinary.config({ 
+        cloud_name: 'dba0qjfeo', 
+        api_key: '483868386424955', 
+        api_secret: 'Yi6UwgxL213LtudtiwIMZjgWQe8' // Click 'View Credentials' below to copy your API secret
+    });
+    
+    // Upload an image
+     const uploadResult = await cloudinary.uploader
+       .upload(
+           'https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg', {
+               public_id: 'shoes',
+           }
+       )
+       .catch((error) => {
+           console.log(error);
+       });
+    
+    console.log(uploadResult);
+    
+    // Optimize delivery by resizing and applying auto-format and auto-quality
+    const optimizeUrl = cloudinary.url('shoes', {
+        fetch_format: 'auto',
+        quality: 'auto'
+    });
+    
+    console.log(optimizeUrl);
+    
+    // Transform the image: auto-crop to square aspect_ratio
+    const autoCropUrl = cloudinary.url('shoes', {
+        crop: 'auto',
+        gravity: 'auto',
+        width: 500,
+        height: 500,
+    });
+    
+    console.log(autoCropUrl);    
+})();
 // const opts = {
 //   overwrite: true,
 //   invalidate: true,
