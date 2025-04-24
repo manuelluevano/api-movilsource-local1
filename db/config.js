1afc34b5b2de9155e6e2c445331738dc4f1a086a
Manuel  Luevano <manuelluevano@Mac-mini-de-Manuel.local>
2025-04-23 23:58:29 -0600

    API V2.4 -solucion a consulta en servidor

--- db/config.js
+++ db/config.js
@@ -1,17 +1,24 @@
-require("dotenv").config();
+//SOLO PARA LOCALHOST
+// require("dotenv").config();
 
 module.exports = {
     app: {
-        port: process.env.PORT || 3306,
+        // port: process.env.PORT || 3306,
+        port: 3306
     },
     jwt: {
-        secret: process.env.JET_SECRET || 'notasecreta!'
+        // secret: process.env.JET_SECRET || 'notasecreta!'
+        secret: 'movilsource'
     },
     mysql: {
-        host: process.env.MYSQL_HOST || "sql3.freesqldatabase.com",
-        user: process.env.MYSQL_USER || "sql3775146",
-        password: process.env.MYSQL_PASSWORD || "MzZps47jrB",
-        databaase: process.env.MYSQL_DB || "sql3775146",
+        // host: process.env.MYSQL_HOST || "sql3.freesqldatabase.com",
+        host: "sql3.freesqldatabase.com",
+        // user: process.env.MYSQL_USER || "sql3775146",
+        user:  "sql3775146",
+        // password: process.env.MYSQL_PASSWORD || "MzZps47jrB",
+        password: "MzZps47jrB",
+        // databaase: process.env.MYSQL_DB || "sql3775146",
+        databaase: "sql3775146"
     }
 } 
 
