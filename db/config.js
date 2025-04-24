05cdcca33c7c764d907b20d0dae0315569ca854b
Manuel  Luevano <manuelluevano@Mac-mini-de-Manuel.local>
2025-04-23 23:48:51 -0600

    API V2.1

--- db/config.js
+++ db/config.js
@@ -2,16 +2,16 @@ require("dotenv").config();
 
 module.exports = {
     app: {
-        port: process.env.PORT || 4000,
+        port: process.env.PORT || 3306,
     },
     jwt: {
         secret: process.env.JET_SECRET || 'notasecreta!'
     },
     mysql: {
-        host: process.env.MYSQL_HOST || "localhost",
-        user: process.env.MYSQL_USER || "root",
-        password: process.env.MYSQL_PASSWORD || "",
-        databaase: process.env.MYSQL_DB || "ejemplo",
+        host: process.env.MYSQL_HOST || "sql3.freesqldatabase.com",
+        user: process.env.MYSQL_USER || "sql3775146",
+        password: process.env.MYSQL_PASSWORD || "MzZps47jrB",
+        databaase: process.env.MYSQL_DB || "sql3775146",
     }
 } 
 
