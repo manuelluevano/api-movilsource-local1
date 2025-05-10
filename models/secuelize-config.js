9244293eeb14708750b24edd818057c9f969fb77
Manuel  Luevano <manuelluevano@Mac-mini-de-Manuel.local>
2025-05-10 00:05:48 -0600

    Cambio de db v2

--- models/secuelize-config.js
+++ models/secuelize-config.js
@@ -2,11 +2,11 @@ const { Sequelize } = require('sequelize');
 
 // Configuración de la base de datos (modifica con tus credenciales)
 const sequelize = new Sequelize(
-  'sql3775146', // Nombre de la DB
-  'sql3775146',                 // Usuario de MySQL (ej: 'root')
-  'MzZps47jrB',              // Contraseña
+  'sql5777999', // Nombre de la DB
+  'sql5777999',                 // Usuario de MySQL (ej: 'root')
+  'x5DJWpgyfs',              // Contraseña
   {
-    host: 'sql3.freesqldatabase.com',          // Host de la DB (puede ser '127.0.0.1')
+    host: 'sql5.freesqldatabase.com',          // Host de la DB (puede ser '127.0.0.1')
     dialect: 'mysql',           // Usamos MySQL
     port: 3306,                 // Puerto por defecto de MySQL
     logging: false,             // Desactiva logs de SQL en consola (opcional)
